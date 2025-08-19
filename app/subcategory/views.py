029e19986dcfcf9203abbadcba0d23eb73c6dc26
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2025-08-19 15:57:38 +0800

    add subcategory filter by category id

--- app/subcategory/views.py
+++ app/subcategory/views.py
@@ -4,13 +4,16 @@ from core.models import SubCategory
 from core.utils import get_basic_filters
 from .serializers import SubCategorySerializer
 
+
 class SubCategoryListView(generics.ListCreateAPIView):
     serializer_class = SubCategorySerializer
 
     def get_queryset(self):
-      filters = get_basic_filters(self.request.query_params)
-
-      return SubCategory.objects.filter(**filters)
+        filters = get_basic_filters(self.request.query_params)
+        category_id = self.request.query_params.get('category_id')
+        if category_id:
+            filters['category'] = category_id
+        return SubCategory.objects.filter(**filters)
 
 
 class SubCategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
